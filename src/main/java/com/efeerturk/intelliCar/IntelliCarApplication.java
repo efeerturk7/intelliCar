6b71b6203e709a6e328f14033ad94e2c38077267
bahadirefeerturk <bahadirefeerturk@gmail.com>
2026-09-23 19:41:01 +0300

    started

---
+++ src/main/java/com/efeerturk/intelliCar/IntelliCarApplication.java
@@ -0,0 +1,13 @@
+package com.efeerturk.intelliCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliCarApplication.class, args);
+	}
+
+}
